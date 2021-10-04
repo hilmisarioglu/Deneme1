bc44b86517539aedac7317d3802f1c398d4ddd3f
HILMI SARIOGLU <77903324+hilmisarioglu@users.noreply.github.com>
2021-10-04 23:00:36 +0200

    Add files via upload
    v1

---
+++ Workshop 1/py.py
@@ -0,0 +1,54 @@
+# a=int(input("a:"))
+# b=int(input("b:"))
+# c=int(input("c:"))  
+
+# output = a * b * c
+
+# print("{}*{}*{} = {}".format(a, b, c, output))
+# print(f"{a}*{b}*{c} = {output}")
+
+# ---------------------------
+# height=float(input("Enter your height (example: 1.85):"))
+# weight=int(input("Enter your weight (example: 75):"))
+
+# print("Body mass index:",weight / (height ** 2))
+
+# ---------------------------
+# totalMoney = 200
+# materialPrice = 11
+# piece = 200 // 11
+# remainingMoney = totalMoney % piece
+# print("With your ${}, {} pieces of material you can get for ${} each.  The remaining money is ${}".format(totalMoney, piece, materialPrice, remainingMoney))
+
+#---------------------------
+# a = 2
+# b = 5
+
+# print("Variables Before Replacing\na= {} b= {}".format(a, b))
+
+# a, b = b, a
+
+# print("Variables After Replacing\na= {} b= {}".format(a, b))
+
+# ---------------------------
+# x, y = 4, 3
+# result = x * x - y * y
+# print("({} ^ 2 - {} ^ 2) = {}".format(x, y, result))
+
+# ---------------------------
+# word = input("Word: ")
+# seperator = input("Seperator: ")
+# repetitions = int(input("repetitions: "))
+# a = (word + seperator) * (repetitions -1) + word # henry/ * 2 = henry/henry/henry
+# print(a)
+
+# ---------------------------
+# print(True and False and (not True and False) and not (True or False))
+
+# ---------------------------
+# print(True and False and not "False" and None and ("None" or None))
+
+# ---------------------------
+# print("clarusway" and 0 and not "" and False and (" " or None))
+
+# print(1 and 2 and 3 and 4 and 5)
